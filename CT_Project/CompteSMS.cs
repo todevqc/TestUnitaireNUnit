27256c127befbecbc865e2beb30a0148433fb9b1
Toufik Programmeur <79265886+todevqc@users.noreply.github.com>
2021-09-26 17:18:09 -0400

    testUnit Twilio SMS

---
+++ CT_Project/CompteSMS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT_Project
+{
+    public class CompteSMS
+    {
+        public IServiceSMS Service { get; set; } = new ServiceSMSTwilio();
+        public List<SMS> SMSEnvoyés { get; set; } = new List<SMS>();
+        public List<SMS> SMSRejetés { get; set; } = new List<SMS>();
+
+        public void ajouterSMSEnvoyé(SMS monSMS)
+        {
+            if (!String.IsNullOrEmpty(monSMS.Destinataire) && !String.IsNullOrEmpty(monSMS.Message))
+            {
+                int resultat = Service.EnvoyerSMS(monSMS);
+
+                if (resultat == 1)
+                {
+                    SMSEnvoyés.Add(monSMS);
+                }
+                else if(resultat == -1)
+                {
+                    SMSRejetés.Add(monSMS);
+                }
+            }
+            else
+            {
+                SMSRejetés.Add(monSMS);
+            }
+        }
+    }
+}
---
+++ CT_Project/IServiceSMS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT_Project
+{
+    public interface IServiceSMS
+    {
+        public int EnvoyerSMS(SMS monSMS);
+    }
+}
